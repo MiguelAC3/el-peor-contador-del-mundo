1eb857d31ff7e62aadab406bf9b9be097256abf6
Miguel Angel Calle Parra <miguel_calle82231@elpoli.edu.co>
2025-08-27 16:31:30 -0500

    se agrega multiplicador

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// Script para aumentar el número al hacer clic en el botón
+// Script para aumentar y multiplicar el número al hacer clic en los botones
 const numeroSpan = document.getElementById('numero');
 const btnIncrementar = document.getElementById('btnIncrementar');
+const btnMultiplicar = document.getElementById('btnMultiplicar');
 let contador = 0;
 
 btnIncrementar.addEventListener('click', () => {
 	contador++;
+	numeroSpan.textContent = contador;
+});
+
+btnMultiplicar.addEventListener('click', () => {
+	contador *= 2;
 	numeroSpan.textContent = contador;
 });
