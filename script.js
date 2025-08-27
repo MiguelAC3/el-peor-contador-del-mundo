46c0a1ed09bcd56545cd05ddd22e5db77a1668a2
Miguel Angel Calle Parra <miguel_calle82231@elpoli.edu.co>
2025-08-27 16:27:45 -0500

    se agrega el descontador

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// Script para aumentar, multiplicar y dividir el número al hacer clic en los botones
+// Script para aumentar el número al hacer clic en el botón
 const numeroSpan = document.getElementById('numero');
 const btnIncrementar = document.getElementById('btnIncrementar');
-const btnMultiplicar = document.getElementById('btnMultiplicar');
-const btnDividir = document.getElementById('btnDividir');
 let contador = 0;
 
 btnIncrementar.addEventListener('click', () => {
 	contador++;
-	numeroSpan.textContent = contador;
-});
-
-btnMultiplicar.addEventListener('click', () => {
-	contador *= 2;
-	numeroSpan.textContent = contador;
-});
-
-btnDividir.addEventListener('click', () => {
-	contador = Math.floor(contador / 2);
 	numeroSpan.textContent = contador;
 });
